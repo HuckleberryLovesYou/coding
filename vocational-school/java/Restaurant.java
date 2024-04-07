8ff8a56ae94a9b50051406efb374bbca24a2a081
Tim Matheis <tim.matheis@outlook.com>
2024-04-07 22:28:39 +0200

    Update Restaurant.java

--- vocational-school/java/Restaurant.java
+++ vocational-school/java/Restaurant.java
@@ -5,72 +5,36 @@ public class Restaurant {
 
 
 
-        boolean gefunden = false;
-        boolean gefunden_1 = false;
-        boolean gefunden_2 = false;
-        boolean gefunden_3 = false;
-        boolean gefunden_4 = false;
-
-
-
-        System.out.println("Ist Pizza vorhanden?");            // frägt User, ob der Schalter 1 aktiv ist
+        System.out.println("Ist Pizza vorhanden?");            // asks user if pizza is available
         Scanner pizza_scanner = new Scanner(System.in);
         boolean pizza = pizza_scanner.nextBoolean();
 
-
-        while (!gefunden )
-            if (pizza) {
-                System.out.println("Der Kunde bekommt eine Pizza");
-                gefunden_1 = true;
-                gefunden = true;
-                System. exit(0);
-            }
-
-
-        System.out.println("Sind Pommes vorhanden?");            // frägt User, ob der Schalter 1 aktiv ist
+        System.out.println("Sind Pommes vorhanden?");            // asks user if french fries is available
         Scanner pommes_scanner = new Scanner(System.in);
         boolean pommes = pommes_scanner.nextBoolean();
 
-        while (!gefunden)
-            if (pommes) {
-                System.out.println("Der Kunde bekommt Pommes");
-                gefunden_2 = true;
-                gefunden = true;
-                System. exit(0);
-            }
-
-
-        System.out.println("Ist Döner vorhanden?");            // frägt User, ob der Schalter 1 aktiv ist
+        System.out.println("Ist Döner vorhanden?");            // asks user if Döner is available
         Scanner Doener_scanner = new Scanner(System.in);
         boolean Doener = Doener_scanner.nextBoolean();
 
-        while (!gefunden)
-            if (Doener) {
-                System.out.println("Der Kunde bekommt einen Döner");
-                gefunden_3= true;
-                gefunden = true;
-                System. exit(0);
-            }
-
-        System.out.println("Ist Salat vorhanden?");            // frägt User, ob der Schalter 1 aktiv ist
+        System.out.println("Ist Salat vorhanden?");            // asks user if salad is available
         Scanner Salat_scanner = new Scanner(System.in);
         boolean Salat = Salat_scanner.nextBoolean();
 
-        while (!gefunden)
-            if (Salat) {
-                System.out.println("Der Kunde bekommt einen Salat");
-                gefunden_4 = true;
-                gefunden = true;
-                System. exit(0);
-            } else System.out.println("Der Kunde bekommt Wasser");
 
 
-
-
-
-
-
-
-
+        if (pizza) {
+            System.out.println("Der Kunde bekommt eine Pizza");
+            System.exit(0);
+        } else if (pommes) {
+            System.out.println("Der Kunde bekommt Pommes");
+            System.exit(0);
+        } else if (Doener) {
+            System.out.println("Der Kunde bekommt einen Döner");
+            System.exit(0);
+        } else if (Salat) {
+            System.out.println("Der Kunde bekommt einen Salat");
+            System.exit(0);
+        } else System.out.println("Der Kunde bekommt Wasser");
     }
 }
