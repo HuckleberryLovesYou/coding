dd154fd9fe8e6895b33b2f49d0adbd397f29395f
Tim Matheis <tim.matheis@outlook.com>
2024-08-13 21:29:13 +0200

    Updated single file version since PasswortManager\PasswordGenerator.py changed

--- vocational-school/python/singleFileProjects/PasswordGenerator.py
+++ vocational-school/python/singleFileProjects/PasswordGenerator.py
@@ -1,28 +1,44 @@
 import random as r
-
-
-
-LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-NUMBERS = "0123456789"
-SPECIAL_CHARACTERS = r"!#$%&'()*+,-./;<=>?@[]^_`{|}~"
+from string import ascii_letters
+from string import digits
+from string import punctuation
 
 #Dependencies:
 #random
+#string.ascii_letters
+#string.digits
+#string.punctuation
 
-def generate_password(password_length):
+LETTERS = ascii_letters #52 chars
+NUMBERS = digits #10 chars
+SPECIAL_CHARACTERS = punctuation #32 chars
+
+def generate_password(password_length, letters=True, numbers=True, special=True):
+    if not letters and not numbers and not special:
+        print("Can't generate a password without any characters")
+        raise ValueError("Can't generate a password without any characters")
+
+    password_length: int = round(password_length) #handles float inputs by rounding it to no decimal places
     password = []
-    for i in range(password_length):
-        list_choice = r.randint(0 , 2)
-        if list_choice == 0:
-            character = LETTERS[r.randint(0, len(LETTERS)-1)]
-            password.append(character)
+    current_password_length: int = 0
+    while password_length > current_password_length:
+        list_choice = r.randint(0 , 2) #generates a value between 0 and 2. This is used to generate evenly often usage of letters, numbers and special characters
+        if list_choice == 0: #LETTERS string has value 0
+            if letters:
+                character = LETTERS[r.randint(0, len(LETTERS)-1)]
+                password.append(character)
+                current_password_length += 1
 
-        elif list_choice == 1:
-            character = NUMBERS[r.randint(0, len(NUMBERS)-1)]
-            password.append(character)
+        elif list_choice == 1: #NUMBERS string has value 1
+            if numbers:
+                character = NUMBERS[r.randint(0, len(NUMBERS)-1)]
+                password.append(character)
+                current_password_length += 1
         else:
-            character = SPECIAL_CHARACTERS[r.randint(0, len(SPECIAL_CHARACTERS)-1)]
-            password.append(character)
+            if special: #SPECIAL_CHARACTERS string has value 2
+                character = SPECIAL_CHARACTERS[r.randint(0, len(SPECIAL_CHARACTERS)-1)]
+                password.append(character)
+                current_password_length += 1
     password_string = ""
     for i in password:
         password_string += i
