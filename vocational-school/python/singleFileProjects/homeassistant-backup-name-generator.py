8a218d7ec03ffe8f0fbac8b2b03a87285945000f
Tim Matheis <tim.matheis@outlook.com>
2024-08-13 14:30:59 +0200

    Changed inputs to use CLI arguments for faster usage

--- vocational-school/python/singleFileProjects/homeassistant-backup-name-generator.py
+++ vocational-school/python/singleFileProjects/homeassistant-backup-name-generator.py
@@ -1,13 +1,16 @@
 import datetime
-
+import argparse
 
 def main():
-    bugfix_version_number: str = input("Enter current bugfix-version-number: ")
-    print(createstring(bugfix_version_number))
+    parser = argparse.ArgumentParser(description="This is a Generator for a reasonable Backup-Name for Homeassistant")
+    parser.add_argument("-v", "--version", required=True, action="store", dest="version", help="Enter current bugfix-version-number [e.g. 2024.8.2 -> 2]")
+    args = parser.parse_args()
 
-def createstring(bugfix_version_number):
+    print(generate_name(args.version))
+
+def generate_name(version=None):
     today: str = f"{datetime.date.today()}"
-    string: str = today + "_Update-" + today[:4] + today[5:7] + bugfix_version_number
+    string: str = today + "_Update-" + today[:4] + today[6:7] + version
     return string
     
 if __name__ == '__main__':
