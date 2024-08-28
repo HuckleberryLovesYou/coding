e2b16afc2815d2a5fbc6b84940267bd808575623
Tim Matheis <tim.matheis@outlook.com>
2024-08-28 22:27:03 +0200

    removed old check_mac_address()

--- vocational-school/python/singleFileProjects/MAC-Address-Converter.py
+++ vocational-school/python/singleFileProjects/MAC-Address-Converter.py
@@ -54,22 +54,6 @@ def is_valid_mac_address(mac_address:  str) -> bool:
         return False
 
 
-def mac_address_vendor(mac_address_vendor_api_mac_address, debug_enabled):  # uses the mac_vendor_api to get the vendor of the mac
-    mac_address_vendor_api_url = "https://api.macvendors.com/" + str(mac_address_vendor_api_mac_address)
-    mac_address_vendor_api_call = requests.get(mac_address_vendor_api_url)
-    if debug_enabled:
-        print("Entering debug mode.\nMore stats will be shown.")
-        print(f"\n\n\n{mac_address_vendor_api_call.status_code=}")
-        print(f"{mac_address_vendor_api_call.elapsed=}")
-        print(f"{mac_address_vendor_api_call.raw=}")
-        print(f"{mac_address_vendor_api_call.reason=}")
-        print(f"{mac_address_vendor_api_call.__hash__()=}\n\n\n")
-    return mac_address_vendor_api_call.text , mac_address_vendor_api_call.status_code
-
-def main():
-    parser = argparse.ArgumentParser("This is a Converter which converts MAC Address Notation to a other one [e.g. 'E8-9C-25-DC-A5-EA' -> 'E8:9C:25:DC:A5:EA']\nIt will also lookup the vendor of the MAC Address\n\n\t©timmatheis-de")
-    parser.add_argument("-m", "--Mac-Address", required=True, action="store", dest="mac_address", help="Provide MAC Address in supported format", type=str)
-    parser.add_argument("-r", "--replace", required=False, default="-", action="store", dest="replace_symbol", help="Enter symbol for replacing [Default: '-']", type=str)
     parser.add_argument("-l", "--lower", required=False, default=False, action="store_true", dest="lower_boolean", help="Specify to change the MAC Address to only lowercase. [Default: False]")
     parser.add_argument("-d", "--debug", required=False, default=False, action="store_true", dest="lower_boolean", help="Specify to enable the debug mode. [Default: False]")
     args = parser.parse_args()
